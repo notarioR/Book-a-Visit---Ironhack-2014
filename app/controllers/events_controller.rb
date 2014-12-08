5866d4d9398b6e380f2dfa42a9fbd307c9adb86e
notarioR <notariorojo@gmail.com>
2014-12-08 14:32:49 +0100

    first commit

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,26 @@
+class EventsController < ApplicationController
+
+	def index
+		@events = Event.all
+	end
+
+	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = Event.new event_params
+		if @event.save
+			flash[:notice] = "Project create successfully"
+			#redirect_to action: 'index', controller: 'entries', project_id: @project.id
+			redirect_to events_path
+		else
+			flash.now[:error] = "Error"
+			render 'new'
+		end
+	end
+
+	def event_params 
+ 		params.require(:event).permit(:title, :description, :date)
+	end
+end
---
+++ db/migrate/20141208133206_add_hour_and_minutes.rb
@@ -0,0 +1,4 @@
+class AddHourAndMinutes < ActiveRecord::Migration
+  def change
+  end
+end
